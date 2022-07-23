a309259668fe49fc404704c07149803bfd4a8a07
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2022-07-23 14:16:00 +0300

    appBar'daki iconButoon'lar oluşturuldu

--- to_do_application/lib/pages/home_pages.dart
+++ to_do_application/lib/pages/home_pages.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,6 +5,31 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppBarBackgroundColor().color,
+      appBar: AppBar(
+        title: Text(
+          AppBarTitleText().text,
+          style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.black),
+        ),
+        centerTitle: false,
+        actions: actionIcon(),
+      ),
+    );
   }
+
+  List<Widget> actionIcon() {
+    return [
+      IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined)),
+      IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+    ];
+  }
+}
+
+class AppBarTitleText {
+  final String text = 'Bugün neler yapacaksın';
+}
+
+class AppBarBackgroundColor {
+  final Color color = Colors.white;
 }
