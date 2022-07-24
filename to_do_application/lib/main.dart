32d2f9f3ab1ea78f2741c797657aa44179212d2b
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2022-07-24 15:22:20 +0300

    hive veritabanının kullanılabilmesi için main metodunda gerekli kodlar yazıldı

--- to_do_application/lib/main.dart
+++ to_do_application/lib/main.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do_application/models/task_model.dart';
+import 'package:to_do_application/pages/home_pages.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskAdapter());
   runApp(MyApp());
-  const SystemUiOverlayStyle(statusBarColor: Colors.transparent);
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           appBarTheme: const AppBarTheme(
               elevation: 0, backgroundColor: Colors.white, iconTheme: IconThemeData(color: Colors.black))),
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
