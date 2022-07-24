929c05633063e75f79e176719763287eb247f076
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2022-07-24 15:36:02 +0300

    get_it paketi eklendi ve merkezi bir yönetim için işlemler yapıldı

--- to_do_application/lib/main.dart
+++ to_do_application/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do_application/data/hive_local_stroge.dart';
+import 'package:to_do_application/data/local_storage.dart';
 import 'package:to_do_application/models/task_model.dart';
 import 'package:to_do_application/pages/home_pages.dart';
+
+final locator = GetIt.instance;
+
+void setup() {
+  locator.registerSingleton<LocalStorage>(HiveLocalStorage());
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +20,7 @@ Future<void> main() async {
 
   await Hive.initFlutter();
   Hive.registerAdapter(TaskAdapter());
+  setup();
   runApp(MyApp());
 }
 
